f87d22dd02007d81da387a2153c01425c53ccc74
Nico <nicoc@ovationsgroup.com>
2020-07-23 11:58:29 +0200

    Added error handling on the front end to handle API down conditions, with a retry

--- react/poc-front-end/src/views/examples/LandingPage.js
+++ react/poc-front-end/src/views/examples/LandingPage.js
@@ -25,6 +25,8 @@ import {
   CardBody,
   CardFooter,
   CardTitle,
+  CardImg,
+  CardText,
   Form,
   Input,
   InputGroupAddon,
@@ -33,6 +35,7 @@ import {
   Container,
   Row,
   Col,
+  Alert,
 } from "reactstrap";
 
 // core components
@@ -46,22 +49,96 @@ import sessionManager from '../../hoc/session-manager';
 class LandingPage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { date: new Date() };
+    this.state = { fortune: null, apiAlert: false };
+    this.retryFetch = this.retryFetch.bind(this);
+    this.fetchFortune = this.fetchFortune.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchFortune();
+  }
+
+  fetchFortune(retryAttempt) {
+    console.log('fetchFortune(): this.state.apiAlert=', this.state.apiAlert);
+    console.log('fetchFortune(): retryAttempt=', retryAttempt);
+    try {
+      if (this.state.apiAlert == false || retryAttempt) {
+        fetch('http://localhost:5000/fortune')
+          .then(response => {
+            let resp = response.json();
+            console.log('resp=', resp)
+            return resp;
+          })
+          .then(data => {
+            console.log('data=', data);
+            this.setState({ fortune: data, apiAlert: false });
+          })
+          .catch((error) => {
+            console.error('Fetch Error:', error);
+            this.setState({ apiAlert: true });
+          });
+      }
+    } catch (e) {
+      console.log('error: ', e);
+    }
+  }
+
+  retryFetch() {
+    console.log('retry fetch');
+    this.setState({apiAlert: false});
+    this.fetchFortune(true);
   }
 
   renderLoginButton() {
+    if (this.state.apiAlert == true) {
+      return (
+        <Button className="btn-round" color="danger" outline onClick={this.retryFetch}>
+          <i className="fa fa-refresh fa-lg" /> Retry connecting to the API
+        </Button>
+      );
+    } else {
+      return (
+        <Button className="btn-round" color="primary" outline>
+          <i className="fa fa-sign-in fa-lg" /> Login to see the secret
+        </Button>
+      );
+    }
+  }
+
+  renderFortune(fortune) {
     return (
-      <Button className="btn-round" color="primary" outline>
-        <i className="fa fa-sign-in fa-lg" />
-        Login to see the secret
-      </Button>
+      <Card style={{ width: '40rem' }}>
+        <CardImg
+          top
+          src={require("assets/img/fortune_cookies.jpg")}
+          alt="Fortune Cookie Image" />
+        <CardBody>
+          <CardTitle>Author: {fortune.fortuneAuthor}</CardTitle>
+          <CardText>{fortune.fortuneText}</CardText>
+          {/* <Button color="primary">Go somewhere</Button> */}
+        </CardBody>
+      </Card>
     );
   }
 
+  renderAlert() {
+    if (this.state.apiAlert == true) {
+      return (
+        <Alert color="danger">
+          Failed to reach the API end-point. Is it up?
+        </Alert>
+      );
+    } else {
+      return <div></div>
+    }
+  }
 
   render() {
 
-    const secretTextHolder = this.renderLoginButton();
+    let secretTextHolder = this.renderLoginButton();
+    if (this.state.fortune !== null) {
+      secretTextHolder = this.renderFortune(this.state.fortune);
+    }
 
     return (
       <>
@@ -72,6 +149,7 @@ class LandingPage extends React.Component {
             <Container>
               <Row>
                 <Col className="ml-auto mr-auto" md="8">
+                  {this.renderAlert()}
                   <h2 className="title">The secret...</h2>
                   <h5 className="description">
                     {secretTextHolder}
